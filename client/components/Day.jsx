ac02581239a1fe1f1130756e9372b51b7b769809
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-09-04 12:55:49 -0700

    final css touches for mobile

--- client/components/Day.jsx
+++ client/components/Day.jsx
@@ -15,10 +15,11 @@ const {astro, date, daily} = props
             <p className="condition">{daily.condition?.text}</p>
             <p className="temp">Sunrise: {astro?.sunrise}</p>
             <p className="feel-temp">Sunset: {astro?.sunset}</p>
-            <p className="uv">High of: {daily?.maxtemp_f}&deg;F</p>
-            <p className="wind-speed">Low of: {daily?.mintemp_f}&deg;F</p>
+            <p className="uv">High: {daily?.maxtemp_f}&deg;F</p>
+            <p className="high">High: {daily?.maxtemp_f}&deg;F</p>
+            <p className="wind-speed">Low: {daily?.mintemp_f}&deg;F</p>
             <p className="wind-dir">Winds up to {daily?.maxwind_mph}MPH</p>
-            <p className="cloud">Possible rain of {daily?.totalprecip_in} inches</p>
+            <p className="cloud">Rain {daily?.totalprecip_in} inches</p>
             <img className="icon" width={100} src={daily.condition?.icon} />
         </div>
     )
