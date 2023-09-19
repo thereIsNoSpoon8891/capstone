db626cbd31b8217d4a3da48858c20a32b946771e
thereIsNoSpoon8891 <130624304+thereIsNoSpoon8891@users.noreply.github.com>
2023-09-19 13:57:27 -0700

    start theme feat.

---
+++ client/context/themeContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext()
+
+const themeContextProvider = props => {
+
+    const { children } = props
+
+
+ 
+return (
+<>
+</>
+)
+}
+ 
+export default themeContextProvider
